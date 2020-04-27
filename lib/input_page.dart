12d9ac46abf97d85cbb984bb4c9e84445038d032
Arafah Ogunfemi <ayobamiarafat09@gmail.com>
2020-04-27 16:52:05 +0100

    'layout'

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,11 +1,17 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+const bottomContainerHeight =80.0;
+const cardBackgroundColor = Color(0xFF1D1E33);
+const buttonColor = Color(0xFFEB1555);
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
 }
 
 class _InputPageState extends State<InputPage> {
+ 
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,51 +23,47 @@ class _InputPageState extends State<InputPage> {
           child: Row(
             children: <Widget>[
               Expanded(
-                  child: Container(
-                margin: EdgeInsets.all(15),
-                decoration: BoxDecoration(
-                    color: Color(0xFF1D1E33),
-                    borderRadius: BorderRadius.circular(10)),
-              )),
+                  child: Card(cardBackgroundColor)),
               Expanded(
-                  child: Container(
-                margin: EdgeInsets.all(15),
-                decoration: BoxDecoration(
-                    color: Color(0xFF1D1E33),
-                    borderRadius: BorderRadius.circular(10)),
-              )),
+                  child: Card(cardBackgroundColor)),
             ],
           ),
         ),
         Expanded(
-          child: Container(
-            margin: EdgeInsets.all(15),
-            decoration: BoxDecoration(
-                color: Color(0xFF1D1E33),
-                borderRadius: BorderRadius.circular(10)),
-          ),
+          child: Card(cardBackgroundColor)
         ),
        Expanded(
           child: Row(
             children: <Widget>[
               Expanded(
-                  child: Container(
-                margin: EdgeInsets.all(15),
-                decoration: BoxDecoration(
-                    color: Color(0xFF1D1E33),
-                    borderRadius: BorderRadius.circular(10)),
-              )),
+                  child: Card(cardBackgroundColor),
+              ),
               Expanded(
-                  child: Container(
-                margin: EdgeInsets.all(15),
-                decoration: BoxDecoration(
-                    color: Color(0xFF1D1E33),
-                    borderRadius: BorderRadius.circular(10)),
-              )),
+                  child: Card(cardBackgroundColor)),
             ],
           ),
         ),
+        Container(
+          color: buttonColor,
+          margin: EdgeInsets.only(top: 10),
+          width: double.infinity,
+          height: bottomContainerHeight,
+        ),
       ]),
     );
+  }
+}
+
+class Card extends StatelessWidget {
+  Card(this.colour);
+final Color colour;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+                margin: EdgeInsets.all(15),
+                decoration: BoxDecoration(
+           color: colour,
+      borderRadius: BorderRadius.circular(10)),
+              );
   }
 }
